9c3def8e74d7b5aa97025c74709b2958585353f4
ngor01 <ngorlouis89@gmail.com>
2022-03-20 14:29:00 +0800

    authentication

--- mayaShop/settings.py
+++ mayaShop/settings.py
@@ -1,3 +1,7 @@
+
+
+
+
 """
 Django settings for mayaShop project.
 
@@ -138,3 +142,15 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configuration'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ngorlouis89@gmail.com'
+EMAIL_HOST_PASSWORD = 'Ngor1996@'
